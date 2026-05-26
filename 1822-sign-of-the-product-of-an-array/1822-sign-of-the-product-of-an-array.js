7cd3d0e807e90722f3ec11d2c26ab3c41191479d
afeefck12345 <afeefck7@gmail.com>
2026-05-26 10:48:37 +0530

    Time: 0 ms (100%), Space: 54.5 MB (60.11%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let product=1;
+    for(let i=0;i<nums.length;i++){
+        product *=nums[i]
+    }
+    if(product>0){
+        return 1
+    }else if(product<0){
+        return -1
+    }
+    else{
+        return 0
+    }
+};
