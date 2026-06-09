16bfdb5092d1ba6c1c6cdd1a3c29eb97c65a1600
afeefck12345 <afeefck7@gmail.com>
2026-06-09 10:03:27 +0530

    Time: 12 ms (70.42%), Space: 60 MB (22.5%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length!==t.length){
+        return false
+    }
+
+    let count={}
+
+    for(let i of s){
+        count[i]=(count[i]||0)+1
+    }
+
+    for(let i of t){
+        if(!count[i])return false;
+        count[i]--
+    }
+    return true
+};
